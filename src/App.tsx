12f8a23bc1b00ca83e0d43fdc1a6f77bfd83ecfb
QuocTrungTDMUD21 <trungdang01012003@gmail.com>
2024-08-19 23:24:21 +0700

    setup something

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import DefaultLayout from './Layout/DefaultLayout/DefaultLayout'
+import { homeRoute } from './Routes'
 
 function App() {
+  
   return (
     <>
-      <p className=''>haha</p>
+      <Routes>
+          <Route path='/' element={<DefaultLayout/>}>
+          {homeRoute.map((route, index) => {return <Route  key={index} path={route.path} element={<route.element></route.element>}/>})}
+          </Route>
+      </Routes>
     </>
   )
 }
