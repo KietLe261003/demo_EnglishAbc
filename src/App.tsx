83f685aaafb249335b605ea3b54190ff275758e4
KietLe <letuankiet26102003kl@gmail.com>
2024-08-20 16:26:57 +0700

    Done task component reports

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
-import DefaultLayout from './Layout/DefaultLayout/DefaultLayout'
-import { homeRoute } from './Routes'
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import DefaultLayout from "./Layout/DefaultLayout/DefaultLayout";
+import { homeRoute } from "./Routes";
 
 function App() {
-  
   return (
     <>
       <Routes>
-          <Route path='/' element={<DefaultLayout/>}>
-          {homeRoute.map((route, index) => {return <Route  key={index} path={route.path} element={<route.element></route.element>}/>})}
-          </Route>
+        <Route path="/" element={<DefaultLayout />}>
+          {homeRoute.map((route, index) => {
+            return (
+              <Route
+                key={index}
+                path={route.path}
+                element={<route.element></route.element>}
+              />
+            );
+          })}
+        </Route>
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
