7d5acdfd8a45ef43efe5c5582e70615200955061
KieuGiaVi <kieugiavi1108thptlvc@gmail.com>
2024-08-21 21:03:31 +0700

    update task1

---
+++ src/Components/Content/Content5.tsx
@@ -0,0 +1,18 @@
+const Content5 = () => {
+    return (
+      <div className="bg-white  w-[584px] h-[275px]">
+        <h2 className="text-2xl font-bold flex items-center text-[40px]">
+          <span className="block w-[8px] h-[40px] bg-orange-400 mr-2 border rounded-md "></span>
+          SIMPLE FUTURE
+        </h2>
+        <p className="mt-8 #34312D text-left text-[20px] w-[584px] h-[88px] ">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
+        </p>
+        <p className="  mt-8  text #34312D text-left text-[20px] w-[584px] h-[88px]">
+          Consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
+        </p>
+      </div>
+    );
+  }
+  
+  export default Content5;
