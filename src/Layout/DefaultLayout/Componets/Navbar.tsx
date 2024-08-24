c14f4b017e6ba0f1ea33b2b4ef3143719351251b
KietLe <letuankiet26102003kl@gmail.com>
2024-08-24 16:06:19 +0700

    Default Layout

--- src/Layout/DefaultLayout/Componets/Navbar.tsx
+++ src/Layout/DefaultLayout/Componets/Navbar.tsx
@@ -3,34 +3,68 @@ import Button from "../../../Components/Button/Button";
 
 const Navbar: React.FC = () => {
   return (
-    <header className="bg-white text-black p-4 left-64 fixed top-0 left-0 right-0 z-10 shadow-md">
-  <div className="container max-w-[1200px] flex flex-wrap items-center">
-    <div className="flex flex-1 space-x-4 justify-end">
-      <div>
-        <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 24 24">
-          <path fill="currentColor" d="M17 18a2 2 0 0 1 2 2a2 2 0 0 1-2 2a2 2 0 0 1-2-2c0-1.11.89-2 2-2M1 2h3.27l.94 2H20a1 1 0 0 1 1 1c0 .17-.05.34-.12.5l-3.58 6.47c-.34.61-1 1.03-1.75 1.03H8.1l-.9 1.63l-.03.12a.25.25 0 0 0 .25.25H19v2H7a2 2 0 0 1-2-2c0-.35.09-.68.24-.96l1.36-2.45L3 4H1zm6 16a2 2 0 0 1 2 2a2 2 0 0 1-2 2a2 2 0 0 1-2-2c0-1.11.89-2 2-2m9-7l2.78-5H6.14l2.36 5z" className="hover:cursor-pointer"></path> 
-        </svg>
-      </div>
-      <div>
-        <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 48 48">
-          <path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2.55} d="M44 24c0 11.046-8.954 20-20 20H4V24C4 12.954 12.954 4 24 4s20 8.954 20 20m-30-6h18m-18 8h18m-18 8h10" className="hover:cursor-pointer"></path>
-        </svg>
-      </div>
-      <div>
-        <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 24 24">
-          <g fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} color="currentColor">
-            <path d="M2.53 14.77c-.213 1.394.738 2.361 1.902 2.843c4.463 1.85 10.673 1.85 15.136 0c1.164-.482 2.115-1.45 1.902-2.843c-.13-.857-.777-1.57-1.256-2.267c-.627-.924-.689-1.931-.69-3.003C19.525 5.358 16.157 2 12 2S4.475 5.358 4.475 9.5c0 1.072-.062 2.08-.69 3.003c-.478.697-1.124 1.41-1.255 2.267"></path>
-            <path d="M8 19c.458 1.725 2.076 3 4 3c1.925 0 3.541-1.275 4-3" className="hover:cursor-pointer"></path>
-          </g>
-        </svg>
-      </div>
-    </div>
-    <div className="ml-3 flex-shrink-0">
-      <Button variant="primary">Đăng nhập</Button>
-    </div>
-  </div>
-</header>
-
+    <header className="  text-black border-none p-4 fixed top-0 left-[355px] right-0 z-10 shadow-md">
+        <div className="flex flex-1 space-x-4 items-center justify-end">
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="1.5em"
+              height="1.5em"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill="currentColor"
+                d="M17 18a2 2 0 0 1 2 2a2 2 0 0 1-2 2a2 2 0 0 1-2-2c0-1.11.89-2 2-2M1 2h3.27l.94 2H20a1 1 0 0 1 1 1c0 .17-.05.34-.12.5l-3.58 6.47c-.34.61-1 1.03-1.75 1.03H8.1l-.9 1.63l-.03.12a.25.25 0 0 0 .25.25H19v2H7a2 2 0 0 1-2-2c0-.35.09-.68.24-.96l1.36-2.45L3 4H1zm6 16a2 2 0 0 1 2 2a2 2 0 0 1-2 2a2 2 0 0 1-2-2c0-1.11.89-2 2-2m9-7l2.78-5H6.14l2.36 5z"
+                className="hover:cursor-pointer"
+              ></path>
+            </svg>
+          </div>
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="1.5em"
+              height="1.5em"
+              viewBox="0 0 48 48"
+            >
+              <path
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2.55}
+                d="M44 24c0 11.046-8.954 20-20 20H4V24C4 12.954 12.954 4 24 4s20 8.954 20 20m-30-6h18m-18 8h18m-18 8h10"
+                className="hover:cursor-pointer"
+              ></path>
+            </svg>
+          </div>
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="1.5em"
+              height="1.5em"
+              viewBox="0 0 24 24"
+            >
+              <g
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={1.5}
+                color="currentColor"
+              >
+                <path d="M2.53 14.77c-.213 1.394.738 2.361 1.902 2.843c4.463 1.85 10.673 1.85 15.136 0c1.164-.482 2.115-1.45 1.902-2.843c-.13-.857-.777-1.57-1.256-2.267c-.627-.924-.689-1.931-.69-3.003C19.525 5.358 16.157 2 12 2S4.475 5.358 4.475 9.5c0 1.072-.062 2.08-.69 3.003c-.478.697-1.124 1.41-1.255 2.267"></path>
+                <path
+                  d="M8 19c.458 1.725 2.076 3 4 3c1.925 0 3.541-1.275 4-3"
+                  className="hover:cursor-pointer"
+                ></path>
+              </g>
+            </svg>
+          </div>
+          <div className="ml-3 flex-shrink-0">
+          <Button variant="primary">Đăng nhập</Button>
+        </div>
+        </div>
+    </header> 
   );
 };
 
