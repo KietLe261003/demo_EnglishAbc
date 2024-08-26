6cc2229fa171120ac209a42385b3c731f68a4e38
KietLe <letuankiet26102003kl@gmail.com>
2024-08-26 21:06:25 +0700

    done code

--- src/Page/ClientPage/HomePage/HomePage.tsx
+++ src/Page/ClientPage/HomePage/HomePage.tsx
@@ -1,35 +1,76 @@
-
 import LessonCard from "../../../Components/CardItem/LessonCard";
 const HomePage = () => {
   return (
-    <div className="flex gap-3">
-      <div className="flex flex-col gap-3">
-        <LessonCard
-          name="Tài Liệu"
-          buttonContent="Xem chi tiết"
-          description="Lorem ipsum dolor sit amet, ipsum consectetuer adipiscing elit...."
-        />
-        <LessonCard
-          name="Tài Liệu"
-          description="Lorem ipsum dolor sit amet, ipsum consectetuer adipiscing elit...."
-        />
-        <LessonCard
-          name="Tài Liệu"
-          description="Lorem ipsum dolor sit amet, ipsum consectetuer adipiscing elit...."
-          border={true}
-        />
+    <div>
+      <div className="flex gap-3">
+        <div className="flex flex-col gap-3">
+          <LessonCard
+            name="Tài Liệu"
+            buttonContent="Xem chi tiết"
+            description="Lorem ipsum dolor sit amet, ipsum consectetuer adipiscing elit...."
+          />
+          <LessonCard
+            name="Tài Liệu"
+            description="Lorem ipsum dolor sit amet, ipsum consectetuer adipiscing elit...."
+          />
+          <LessonCard
+            name="Tài Liệu"
+            description="Lorem ipsum dolor sit amet, ipsum consectetuer adipiscing elit...."
+            border={true}
+          />
+        </div>
+        <div className="flex flex-col gap-3">
+          <LessonCard
+            name="Tài Liệu"
+            price={20}
+            description="Lorem ipsum dolor sit amet, ipsum consectetuer adipiscing elit...."
+          />
+          <LessonCard
+            name="Tài Liệu"
+            price={20}
+            description="Lorem ipsum dolor sit amet, ipsum consectetuer adipiscing elit...."
+            percent={30}
+          />
+        </div>
+        <div className="flex flex-col gap-3">
+          <LessonCard
+            name="Tài Liệu"
+            buttonContent="Làm bài"
+            description="Lorem ipsum dolor sit amet, ipsum consectetuer adipiscing elit...."
+          />
+          <LessonCard
+            name="Tài Liệu"
+            buttonContent="Làm bài"
+            state="Pass"
+            description="Lorem ipsum dolor sit amet, ipsum consectetuer adipiscing elit...."
+          />
+          <LessonCard
+            name="Tài Liệu"
+            buttonContent="Làm bài"
+            state="Faill"
+            description="Lorem ipsum dolor sit amet, ipsum consectetuer adipiscing elit...."
+          />
+          <LessonCard
+            name="Tài Liệu"
+            buttonContent="Làm bài"
+            state="Cần làm"
+            description="Lorem ipsum dolor sit amet, ipsum consectetuer adipiscing elit...."
+          />
+        </div>
+        <div className="flex flex-col gap-3">
+          <LessonCard
+            name="Tài Liệu"
+            price={20}
+            description="Lorem ipsum dolor sit amet, ipsum consectetuer adipiscing elit...."
+            type={false}
+          />
+        </div>
       </div>
       <div className="flex flex-col gap-3">
         <LessonCard
           name="Tài Liệu"
           price={20}
           description="Lorem ipsum dolor sit amet, ipsum consectetuer adipiscing elit...."
-        />
-        <LessonCard
-          name="Tài Liệu"
-          price={20}
-          description="Lorem ipsum dolor sit amet, ipsum consectetuer adipiscing elit...."
-          percent={30}
         />
       </div>
       <div className="flex flex-col gap-3">
@@ -66,47 +107,6 @@ const HomePage = () => {
         />
       </div>
     </div>
-    <div className="flex flex-col gap-3">
-      <LessonCard
-        name="Tài Liệu"
-        price={20}
-        description="Lorem ipsum dolor sit amet, ipsum consectetuer adipiscing elit...."
-      />
-    </div>
-    <div className="flex flex-col gap-3">
-      <LessonCard
-        name="Tài Liệu"
-        buttonContent="Làm bài"
-        description="Lorem ipsum dolor sit amet, ipsum consectetuer adipiscing elit...."
-      />
-      <LessonCard
-        name="Tài Liệu"
-        buttonContent="Làm bài"
-        state="Pass"
-        description="Lorem ipsum dolor sit amet, ipsum consectetuer adipiscing elit...."
-      />
-      <LessonCard
-        name="Tài Liệu"
-        buttonContent="Làm bài"
-        state="Faill"
-        description="Lorem ipsum dolor sit amet, ipsum consectetuer adipiscing elit...."
-      />
-      <LessonCard
-        name="Tài Liệu"
-        buttonContent="Làm bài"
-        state="Cần làm"
-        description="Lorem ipsum dolor sit amet, ipsum consectetuer adipiscing elit...."
-      />
-    </div>
-    <div className="flex flex-col gap-3">
-      <LessonCard
-        name="Tài Liệu"
-        price={20}
-        description="Lorem ipsum dolor sit amet, ipsum consectetuer adipiscing elit...."
-        type={false}
-      />
-    </div>
-  </div>
   );
 };
 
