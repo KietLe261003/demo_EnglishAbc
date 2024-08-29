c26713806c11114d153f5a357e95e8a03956a8a9
KieuGiaVi <kieugiavi1108thptlvc@gmail.com>
2024-08-29 20:09:42 +0700

    fix CheckButton

--- src/Components/Button/CheckButton.tsx
+++ src/Components/Button/CheckButton.tsx
@@ -1,31 +1,38 @@
 import React, { useState } from 'react';
+import clsx from 'clsx';
 
 interface CheckButtonProps {
-    labels: string[];
+    data: { label: string; key: string }[];
+    onChange: (key: string | null) => void;
 }
 
-const CheckButton: React.FC<CheckButtonProps> = ({ labels }) => {
+const CheckButton: React.FC<CheckButtonProps> = ({ data, onChange }) => {
     const [selectedIndex, setSelectedIndex] = useState<number | null>(null);
 
     const handleCheckboxClick = (index: number) => {
         setSelectedIndex(index);
+        onChange(data[index]?.key || null);
     };
 
     return (
         <div className="p-6 space-y-4">
-            {labels.map((label, index) => (
+            {data.map((item, index) => (
                 <div
-                    key={index}
+                    key={item.key}
                     className="flex items-center space-x-2 cursor-pointer"
                     onClick={() => handleCheckboxClick(index)}
                 >
-                    <div 
-                        className={`w-6 h-6 rounded-md border-2 ${
-                            selectedIndex === index ? 'bg-orange-700 border-transparent' : 'bg-white border-orange-500'
-                        }`}
+                    <div
+                        className={clsx(
+                            'w-6 h-6 rounded-md border-2',
+                            selectedIndex === index ? 'bg-orange-700 border-transparent' : 'bg-white border-orange-700'
+                        )}
                     />
-                    <span className={`text-[12px]  ${selectedIndex === index ? 'text-orange-700' : 'text-gray-800'}`}>
-                        {label}
+                    <span className={clsx(
+                        'text-[12px]',
+                        selectedIndex === index ? 'text-orange-700' : 'text-gray-800'
+                    )}>
+                        {item.label}
                     </span>
                 </div>
             ))}
