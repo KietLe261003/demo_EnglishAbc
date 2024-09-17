7cb82b9c339708cabf805be953a0a8516c4bcab1
KietLe261003 <letuankiet26102003kl@gmail.com>
2024-09-17 09:34:52 +0700

    Fix ckeditor

--- src/Page/ClientPage/CourseOfflinePage/Components/Content/ContentInfomation.tsx
+++ src/Page/ClientPage/CourseOfflinePage/Components/Content/ContentInfomation.tsx
@@ -1,61 +1,61 @@
-import Editor from 'ckeditor5-custom-build/build/ckeditor';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
-const editorConfiguration = {
-  toolbar: {
-    items: [
-      'undo',
-      'redo',
-      '|',
-      'heading',
-      '|',
-      'fontfamily',
-      'fontsize',
-      'fontColor',
-      'fontBackgroundColor',
-      '|',
-      'bold',
-      'italic',
-      'strikethrough',
-      'subscript',
-      'superscript',
-      'code',
-      '-', // break point
-      '|',
-      'alignment',
-      'link',
-      'uploadImage',
-      'blockQuote',
-      'codeBlock',
-      '|',
-      'bulletedList',
-      'numberedList',
-      'todoList',
-      'outdent',
-      'indent',
-    ],
-    shouldNotGroupWhenFull: true,
-  },
-};
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic'; 
 
 function ContentInformation() {
   return (
     <>
       <CKEditor
-        editor={Editor as any} 
-        config={editorConfiguration}
-        data='<p>Hello from CKEditor&nbsp;5!</p>'
-        onReady={(editor) => {
-          // You can store the "editor" and use when it is needed.
-          console.log('Editor is ready to use!', editor);
+        editor={ClassicEditor}
+        config={{
+          toolbar: {
+            items: [
+              'undo',
+              'redo',
+              '|',
+              'heading',
+              '|',
+              'fontSize',
+              'fontColor',
+              'fontBackgroundColor',
+              '|',
+              'bold',
+              'italic',
+              'strikethrough',
+              'subscript',
+              'superscript',
+              'code',
+              '-',
+              '|',
+              'alignment',
+              'link',
+              'imageUpload',
+              'blockQuote',
+              'codeBlock',
+              '|',
+              'bulletedList',
+              'numberedList',
+              'todoList',
+              'outdent',
+              'indent',
+            ],
+          },
+          image: {
+            toolbar: [
+              'imageTextAlternative',
+              'imageStyle:full',
+              'imageStyle:side',
+            ],
+          },
+          initialData: '<p>Hello from CKEditor 5 in React!</p>',
         }}
         onChange={(event, editor) => {
-          console.log('Blur.', editor,event);
+          console.log('Blur.', editor, event);
         }}
         onBlur={(event, editor) => {
-          console.log('Blur.', editor,event);
+          console.log('Blur.', editor, event);
         }}
         onFocus={(event, editor) => {
-          console.log('Focus.', editor,event);
+          console.log('Focus.', editor, event);
         }}
       />
     </>
--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['ckeditor5-custom-build/build/ckeditor'],
-  },
-  build: {
-    commonjsOptions: {
-      include: ['ckeditor5-custom-build/build/ckeditor'],
-    },
-  },
 })
