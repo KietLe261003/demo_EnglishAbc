a8f2581320c0f54c558642df7d34dbea38d9a8c4
KietLe <letuankiet26102003kl@gmail.com>
2024-09-25 14:15:29 +0700

    Done fix

--- src/Common/Config/Request.ts
+++ src/Common/Config/Request.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
-
+const apiUrl=import.meta.env.URL_REQUEST;
 export const request = axios.create({
-    baseURL: "http://localhost:8080/",
+    baseURL: apiUrl,
     timeout: 1000,
     headers: {'X-Custom-Header': 'foobar'}
 })
--- src/Common/Context/AuthContext.tsx
+++ src/Common/Context/AuthContext.tsx
@@ -1,35 +1,36 @@
-import { User } from "ckeditor5-premium-features";
-import { createContext, PropsWithChildren, useContext, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { RouterLink } from "../../Util/RouterLink";
-interface AuthContextType{
-    user: User | undefined,
-    login: (user:User)=>void,
-    logout: ()=>void
+import { User } from 'ckeditor5-premium-features';
+import { createContext, PropsWithChildren, useContext, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { RouterLink } from '../../Util/RouterLink';
+interface AuthContextType {
+  user: User | null | undefined;
+  login: (user: User) => void;
+  logout: () => void;
 }
-export const AuthConext = createContext<AuthContextType | undefined>(undefined)
+const USER_KEY=import .meta.env.USER_KEY;
+export const AuthConext = createContext<AuthContextType | null | undefined>(undefined);
 
-export const AuthContextProvider = ({children}:PropsWithChildren)=>{
-    const navigate=useNavigate();
-    const [user,setUser]=useState<User | undefined>(undefined);
-    const login = (userData:User)=>{
-        localStorage.setItem("User",JSON.stringify(userData));
-        setUser(userData);
-        alert("Đăng nhập thành công")
-        navigate(RouterLink.Home)
-    }
-    const logout = ()=>{
-
-    }
-    return (
-        <AuthConext.Provider value={{user,login,logout}}>
-            {children}
-        </AuthConext.Provider>
-    )
-}
-export const useAuth = ()=>{
-    const context=useContext(AuthConext);
-    if(!context)
-        throw new Error("User này bị rỗng");
-    return context;
-} 
+export const AuthContextProvider = ({ children }: PropsWithChildren) => {
+  const navigate = useNavigate();
+  const [user, setUser] = useState<User | null>(() => {
+    const storedUser = localStorage.getItem(USER_KEY);
+    return storedUser ? JSON.parse(storedUser) : undefined;
+  });
+  const login = (userData: User) => {
+    localStorage.setItem('User', JSON.stringify(userData));
+    setUser(userData);
+    alert('Đăng nhập thành công');
+    navigate(RouterLink.Home);
+  };
+  const logout = () => {};
+  return (
+    <AuthConext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthConext.Provider>
+  );
+};
+export const useAuth = () => {
+  const context = useContext(AuthConext);
+  if (!context) throw new Error('User này bị rỗng');
+  return context;
+};
